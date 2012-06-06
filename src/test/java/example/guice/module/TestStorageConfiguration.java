b8542e5c90f4ce0f8f035b68bbd2368285ffbfd0
Ghenadii Batalski <Ghenadii.Batalski@student.kit.edu>
2012-06-06 22:59:34 +0200

    Whitespaces test for snakeyaml

--- src/test/java/example/guice/module/TestStorageConfiguration.java
+++ src/test/java/example/guice/module/TestStorageConfiguration.java
@@ -16,11 +16,19 @@ public class TestStorageConfiguration {
 	@Test
 	public void testToYaml() {
 		CassandraStorage cs = new CassandraStorage();
+		cs.setHosts(new String[] { "184.72.179.92", "184.72.179.91" });
 		cs.toYaml();
 		assertEquals(	cs,
 						CassandraStorage.fromYaml("!!example.guice.module.CassandraModule$CassandraStorage\n"
 				+ "cluster: Nebula Cassandra Cluster\n"
-				+ "hosts: [localhost]\n" + "keyspace: Twissjava\n"
+								+ "hosts: [184.72.179.92, 184.72.179.91]\n"
+								+ "keyspace: Twissjava\n"
 							+ "port: 9160\n"));
-		}
+
+		cs = CassandraStorage.fromYaml("!!example.guice.module.CassandraModule$CassandraStorage\n"
+				+ "cluster: Nebula Cassandra Cluster\n"
+				+ "hosts: [localhost, huj]\n"
+				+ "keyspace: Twissjava\n"
+				+ "port: 9160\n");
+	}
 }
